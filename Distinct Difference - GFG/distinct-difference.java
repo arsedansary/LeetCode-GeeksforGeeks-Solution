d4738bec5163f6f5a99588ed5d8586f124fa5678
ARSED ANSARY <90137553+arsedansary@users.noreply.github.com>
2023-03-19 07:31:41 +0530

    Added solution - LeetHub

---
+++ Distinct Difference - GFG/distinct-difference.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.getDistinctDifference(N, A);
+            
+            IntArray.print(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> getDistinctDifference(int N, int[] A) {
+        // code here
+        ArrayList<Integer> a=new ArrayList<>();
+        int B[]=new int[N];
+        HashSet<Integer> m=new HashSet<>();
+        
+        for(int i=N-1;i>=0;i--)
+        {
+            B[i]=m.size();
+            m.add(A[i]);
+        }
+        m.clear();
+        for(int i=0;i<N;i++)
+        {
+            a.add(m.size()-B[i]);
+            m.add(A[i]);
+        }
+        
+        return a;
+    }
+}
+  
