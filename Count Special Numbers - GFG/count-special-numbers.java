83aee99e835ffad2f81a4b24e52cc5c9e72c8d09
ARSED ANSARY <90137553+arsedansary@users.noreply.github.com>
2023-04-05 07:12:10 +0530

    Added solution - LeetHub

---
+++ Count Special Numbers - GFG/count-special-numbers.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt(); // Inputting the testcases
+        PrintWriter out = new PrintWriter(System.out);
+        while (t-- > 0) {
+
+            int i = 0;
+            int N = sc.nextInt();
+
+            int arr[] = new int[(int)(N)];
+
+            for (i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Solution ob = new Solution();
+            out.println(ob.countSpecialNumbers(N, arr));
+        }
+        out.flush();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int countSpecialNumbers(int N, int arr[]) {
+        // Code here
+         HashMap<Integer,Integer>map=new HashMap<Integer,Integer>();
+        for(int i=0;i<N;i++)
+        {
+            if(map.get(arr[i])==null)
+            map.put(arr[i],1);
+            else
+            map.put(arr[i],map.get(arr[i])+1);
+        }
+        int count =0;
+        for(int i=0;i<N;i++)
+        {
+            if(map.containsKey(arr[i]))
+            {
+            for(int j=0;j<N;j++)
+            {
+                if(i!=j&&arr[i]%arr[j]==0)
+                {
+                    count=count+map.get(arr[i]);
+                    map.remove(arr[i]);
+                    break;
+                }
+            }
+            }
+        }
+        return count;
+
+ 
+    }
+}
