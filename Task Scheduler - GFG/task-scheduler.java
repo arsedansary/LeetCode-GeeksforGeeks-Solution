9cfa032a8860670b5760cda33a4eb87b5e8e9cd2
ARSED ANSARY <90137553+arsedansary@users.noreply.github.com>
2023-06-23 20:51:50 +0530

    Added solution - LeetHub

--- Task Scheduler - GFG/task-scheduler.java
+++ Task Scheduler - GFG/task-scheduler.java
@@ -91,46 +91,16 @@ class GFG {
 class Solution {
     static int leastInterval(int N, int K, char tasks[]) {
         // code here
-        int[] freq = new int[26];
-        
-        for (int task : tasks) {
-            freq[task - 'A']++;
+        int f[] = new int [26];
+        for(char task: tasks){
+            f[task-'A']++;
         }
-        
-        int[] activationTime = new int[26];
-        
-        int cnt = 0;
-        int time = 0;
-        int maxFreq = -1;
-        
-        while (cnt < N) {
-            maxFreq = getMax(freq, activationTime, time);
-            
-            if (maxFreq > -1) {
-                cnt++;
-                freq[maxFreq]--;
-                activationTime[maxFreq] = time + K + 1;
-            }
-            
-            // System.out.print (maxFreq + " ");
-            
-            time++;
+        Arrays.sort(f);
+        int maxfre = f[25]-1;
+        int idle = (maxfre)*K;
+        for(int i=24;i>=0;i--){
+            idle-=Math.min(maxfre,f[i]);
         }
-        
-        return time;
-    }
-    
-    private static int getMax(int[] freq, int[] activationTime, int currTime) {
-        int maxFreq = 0;
-        int idx = -1;
-        
-        for (int i = 0 ; i < 26 ; i++) {
-            if (freq[i] > maxFreq && activationTime[i] <= currTime) {
-                maxFreq = freq[i];
-                idx = i;
-            }
-        }
-        
-        return idx;
+        return (idle<0)? tasks.length: idle+tasks.length;
     }
 }
