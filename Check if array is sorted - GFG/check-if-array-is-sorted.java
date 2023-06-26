2ab00b46072d25577e43e74ae66bccd67ff1ab81
ARSED ANSARY <90137553+arsedansary@users.noreply.github.com>
2023-06-26 17:26:08 +0530

    Added solution - LeetHub

--- Check if array is sorted - GFG/check-if-array-is-sorted.java
+++ Check if array is sorted - GFG/check-if-array-is-sorted.java
@@ -31,12 +31,12 @@ import java.io.*;
 
 class Solution {
     boolean arraySortedOrNot(int[] arr, int n) {
-        // code here
-        for (int i=1; i<n; i++){
-        if (arr [i] < arr[i-1] ){
-                return false;
-            }
+        // code herear]
+        for (int i=1; i <n; i++){
+            if (arr [i] < arr[i-1])
+               return false;
         }
-        return true;
+           return true;
     }
-}
+  
+ }
